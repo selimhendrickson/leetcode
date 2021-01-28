2bac970f0ae53daa17d8059421a58bd1942249b2
Selim Hendrickson <selimhendrickson@gmail.com>
2021-01-27 17:51:09 -0800

    updated gitignore to remove temp.py

--- temp.py
+++ temp.py
@@ -1,44 +1,26 @@
+def binary_search(arr, key):
+  #start in the middle
+  index = len(arr) // 2
 
-class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
-
-
-def find_unique_trees(n):
-  if n <= 0:
-    return []
-  return findUnique_trees_recursive(1, n)
-
-
-def findUnique_trees_recursive(start, end):
-  result = []
-  # base condition, return 'None' for an empty sub-tree
-  # consider n = 1, in this case we will have start = end = 1, this means we should have only one tree
-  # we will have two recursive calls, findUniqueTreesRecursive(1, 0) & (2, 1)
-  # both of these should return 'None' for the left and the right child
-  if start > end:
-    result.append(None)
-    return result
-
-  for i in range(start, end+1):
-    # making 'i' the root of the tree
-    leftSubtrees = findUnique_trees_recursive(start, i - 1)
-    rightSubtrees = findUnique_trees_recursive(i + 1, end)
-    for leftTree in leftSubtrees:
-      for rightTree in rightSubtrees:
-        root = TreeNode(i)
-        root.left = leftTree
-        root.right = rightTree
-        result.append(root)
-
-  return result
-
+  #if index goes above or below, the key doesn't exist
+  while index > -1 and index < len(arr):
+    num = arr[index]
+    if num == key: return index
+    if num < key:
+      if len(arr) - index <= 2: index += 1
+      else:
+        index += index // 2 
+    if num > key: 
+      if index <= 1: index = 0
+      else:
+        index = index // 2 
+  return -1
 
 def main():
-  print("Total trees: " + str(len(find_unique_trees(2))))
-  print("Total trees: " + str(len(find_unique_trees(3))))
+  print(binary_search([4, 6, 10], 10))
+  print(binary_search([1, 2, 3, 4, 5, 6, 7], 5))
+  print(binary_search([10, 6, 4], 10))
+  print(binary_search([10, 6, 4], 4))
 
 
 main()
